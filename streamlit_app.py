7e5cb7d256cadb600ae670ea364ffd678ee0db11
zhermin <tamzhermin@gmail.com>
2021-10-24 01:53:53 +0800

    added batch upload feature

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,19 +5,30 @@ import matplotlib.pyplot as plt
 import os, glob, time
 from PIL import Image
 
+import tensorflow as tf
 from keras.models import load_model
 from keras.applications import vgg16
 
-np.set_printoptions(suppress=True)
+#------------------------------------------------------------------------------#
 
 IMG_SIZE = 256
+BATCH_SIZE = 16
 DEFECT_LIST = ['none', 'chipping']
 DEFECT_MAPPING = dict(enumerate(DEFECT_LIST))
 
+#------------------------------------------------------------------------------#
+
 @st.cache
-def load_image(image_file):
-	img = Image.open(image_file)
-	return img
+def load_images(image_files):
+    for i in range(len(image_files)):
+        img = Image.open(image_files[i])
+        img = img.resize((IMG_SIZE, IMG_SIZE))
+        img = np.asarray(img)[:, :, :3]
+        image_files[i] = img
+
+    image_files = np.asarray(image_files)
+    image_files = vgg16.preprocess_input(image_files)
+    return image_files
 
 @st.cache(allow_output_mutation=True)
 def model_load(model_name):
@@ -26,9 +37,20 @@ def model_load(model_name):
     loaded_model.summary()
     return loaded_model
 
+@st.cache(show_spinner=False)
+def predict(imgs):
+    with tf.device('/cpu:0'):
+        pred = model.predict(imgs, batch_size=1)
+
+    df_pred = pd.DataFrame(pred, columns=DEFECT_LIST)
+    df_pred['prediction'] = np.argmax(pred, axis=1)
+    df_pred['prediction'] = df_pred['prediction'].map(DEFECT_MAPPING.get)
+    return df_pred
+
+#------------------------------------------------------------------------------#
 
 st.write("""
-# Wafer Edge ADC using CNN
+# CNN for Wafer Edge ADC
 Latest Results: 99.99% Out of Sample Accuracy (4571/4577)
 
 ---
@@ -44,6 +66,8 @@ model_name = st.sidebar.selectbox(
     index=len(model_names)-1,
 )
 
+st.write(f"## Model Selected: {model_name}")
+st.write("")
 
 model = None
 model = model_load(model_name)
@@ -51,14 +75,26 @@ model = model_load(model_name)
 if model == None:
     st.write("Model not loaded yet")
 
-st.write(f"## Image Upload")
+#------------------------------------------------------------------------------#
 
-image_file = st.file_uploader("", type=['png','jpeg','jpg'])
+# st.write(f"## Image Upload")
 
-if image_file is not None:
+# image_files = st.file_uploader("", type=['png','jpeg','jpg'], accept_multiple_files=True)
+# st.write(image_files)
+
+with st.form("image-uploader", clear_on_submit=True):
+    image_files = st.file_uploader("IMAGE UPLOADER", type=['png','jpeg','jpg'], accept_multiple_files=True)
+    submitted = st.form_submit_button("UPLOAD BATCH")
+
+    if submitted:
+        if len(image_files) > 0:
+            st.success(f"{len(image_files)} FILE(S) UPLOADED!")
+        else:
+            st.info("FILES CLEARED")
+
+if len(image_files) > 0:
 
     # st.write(dir(image_file))
-
     # file_details = {
     #     "Filename":image_file.name,
     #     "FileType":image_file.type,
@@ -66,22 +102,16 @@ if image_file is not None:
     # }
     # st.write(file_details)
 
-    img = load_image(image_file)
-    st.image(img, width=256)
-
-    img = img.resize((256, 256))
-    img = np.asarray(img)[:, :, :3]
-    img = vgg16.preprocess_input(img)
-    img = np.expand_dims(img, axis=0)
-
-    st.write(f"---")
-    st.write(f"## Single Image Prediction")
+    imgs = load_images(image_files)
+    # st.image(img, width=IMG_SIZE)
 
     with st.spinner('Predicting...'): 
         start = time.time()
-        pred = model.predict(img)
-    df_pred = pd.DataFrame(pred, columns=DEFECT_LIST)
+        df_pred = predict(imgs)
+
+    st.write(f"---")
+    st.write(f"## Image Predictions")
 
     st.write(df_pred)
-    st.write(f'Prediction for this image is: {DEFECT_MAPPING.get(np.argmax(pred, axis=1)[0])} ({np.max(pred):.2%} confident)')
+    # st.write(f'Prediction for this image is: {DEFECT_MAPPING.get(np.argmax(pred, axis=1)[0])} ({np.max(pred):.2%} confident)')
     st.write(f'Runtime: {round((time.time() - start)/60, 2)} mins')
