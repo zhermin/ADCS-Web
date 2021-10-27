65290d43d645f5e32a1ef8e3e215f7b85f152604
zhermin <tamzhermin@gmail.com>
2021-10-27 17:45:55 +0800

    add expanders for cleaner look and edited page configs

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,22 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import os, glob, time
 from PIL import Image
 
-import tensorflow as tf
 from keras.models import load_model
 from keras.applications import vgg16
 
-st.set_page_config(layout="wide")
+st.set_page_config(
+    page_title="Wafer Edge ADC",
+    page_icon="🤖",
+    layout="wide",
+    menu_items={
+        'Get Help': None,
+        'Report a bug': None,
+        'About': "### This is an MVP of an ML Model Deployment UI\nDisclaimer: App is not optimized for performance nor integrated with any internal company infrastructure"
+    }
+)
 
 hide_streamlit_style = """
 <style>
@@ -19,14 +26,14 @@ footer {visibility: hidden;}
 """
 # st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
-#------------------------------------------------------------------------------#
+#----------------------------------- Constants --------------------------------#
 
 IMG_SIZE = 256
 BATCH_SIZE = 1
 DEFECT_LIST = ['none', 'chipping']
 DEFECT_MAPPING = dict(enumerate(DEFECT_LIST))
 
-#------------------------------------------------------------------------------#
+#----------------------------------- Functions --------------------------------#
 
 @st.cache(allow_output_mutation=True)
 def model_load(model_name):
@@ -35,7 +42,6 @@ def model_load(model_name):
     loaded_model.summary()
     return loaded_model
 
-# @st.cache
 def load_images(image_files):
     imgs = []
     for i in range(len(image_files)):
@@ -50,8 +56,7 @@ def load_images(image_files):
 
 @st.cache(show_spinner=False)
 def predict(image_files):
-    with tf.device('/cpu:0'):
-        pred = model.predict(load_images(image_files), batch_size=BATCH_SIZE)
+    pred = model.predict(load_images(image_files), batch_size=BATCH_SIZE)
     return pred
 
 @st.cache
@@ -61,13 +66,15 @@ def load_df(pred, threshold):
     df_pred['unconfident'] = np.where(df_pred['confidence'] < threshold, True, False)
     df_pred['prediction'] = np.argmax(pred, axis=1)
     df_pred['prediction'] = df_pred['prediction'].map(DEFECT_MAPPING.get)
+    df_pred.insert(0, 'name', [image_file.name for image_file in image_files])
 
+    df_none = df_pred.loc[df_pred['prediction'] == 'none']
     df_chipping = df_pred.loc[df_pred['prediction'] == 'chipping']
 
     df_unconfident = df_pred.loc[df_pred['unconfident'] == True]
     df_unconfident = df_unconfident.drop('unconfident', axis=1)
 
-    return df_pred, df_chipping, df_unconfident
+    return df_pred, df_none, df_chipping, df_unconfident
 
 def highlight_pred(image_files, df, max_cols):
     idx = df.index
@@ -82,13 +89,13 @@ def highlight_pred(image_files, df, max_cols):
         st_row = st.columns(max_cols)
         for col in range(num_cols):
             image_file = image_files[idx[row*max_cols+col]]
-            st_row[col].image(image_file)
-
             selected_row = df.iloc[row*max_cols+col]
-            st_row[col].write(f'#### [{idx[row*max_cols+col]}] {image_file.name}')
-            st_row[col].write(f'###### {selected_row["prediction"]} ({selected_row["confidence"]:.2%})')
 
-#------------------------------------------------------------------------------#
+            st_row[col].image(image_file)
+            st_row[col].write(f'<p style="text-align: center;">[{idx[row*max_cols+col]}] {image_file.name}</p>', unsafe_allow_html=True)
+            st_row[col].write(f'<p style="text-align: center;">{selected_row["prediction"]} ({selected_row["confidence"]:.2%})</p>', unsafe_allow_html=True)
+
+#------------------------------- Sidebar & Headers ----------------------------#
 
 st.write("""
 # CNN for Wafer Edge ADC
@@ -107,12 +114,12 @@ model_name = st.sidebar.selectbox(
     index=len(model_names)-1,
 )
 
-threshold = st.sidebar.number_input(
+threshold = st.sidebar.slider(
     label='Select Confidence % Threshold',
-    min_value=80.00,
-    max_value=100.00,
-    value=90.00,
-    step=0.01,
+    min_value=80,
+    max_value=100,
+    value=95,
+    step=1,
     # format='%.2f'
 )
 threshold /= 100
@@ -133,7 +140,7 @@ model = model_load(model_name)
 if model == None:
     st.write("Model not loaded yet")
 
-#------------------------------------------------------------------------------#
+#--------------------------------- Uploader Form ------------------------------#
 
 with st.form("image-uploader", clear_on_submit=True):
     image_files = st.file_uploader("UPLOAD IMAGES TO PREDICT", type=['png','jpeg','jpg'], accept_multiple_files=True)
@@ -145,42 +152,44 @@ with st.form("image-uploader", clear_on_submit=True):
         else:
             st.info("IMAGES CLEARED")
 
+#---------------------------------- PREDICTIONS  ------------------------------#
+
 if len(image_files) > 0:
-
-    # st.write(dir(image_file))
-    # file_details = {
-    #     "Filename":image_file.name,
-    #     "FileType":image_file.type,
-    #     "FileSize":image_file.size
-    # }
-    # st.write(file_details)
-
-    # imgs = load_images(image_files)
 
     with st.spinner(f'Predicting {len(image_files)} Images...'): 
         start = time.time()
         pred = predict(image_files)
 
-    df_pred, df_chipping, df_unconfident = load_df(pred, threshold)
+    df_pred, df_none, df_chipping, df_unconfident = load_df(pred, threshold)
 
-    #------------------------------------------------------------------------------#
+    #-------------------------------- All Predictions -----------------------------#
 
     st.write(f"---")
-    st.write(f"## {len(image_files)} Image Predictions")
+    st.write(f"## {len(image_files)} Image Predictions (Runtime: {round((time.time() - start)/60, 2)} mins)")
 
-    st.dataframe(df_pred.style.highlight_max(color='grey', axis=1))
-    st.write(f'Runtime: {round((time.time() - start)/60, 2)} mins')
+    with st.expander(f'View Table of All Predictions'):
+        st.dataframe(df_pred.style.apply(
+            lambda df: ['background: grey' if df['prediction'] == 'chipping' else '' for row in df], 
+            axis=1
+        ))
 
-    #------------------------------------------------------------------------------#
+    #------------------------------- None Predictions -----------------------------#
 
-    st.write(f"---\n## {len(df_chipping)} Chipping Predictions")
-    st.dataframe(df_chipping)
-    st.write("")
-    highlight_pred(image_files, df_chipping, max_cols)
+    with st.expander(f'View {len(df_none)} None Predictions'):
+        st.dataframe(df_none)
+        st.write("")
+        highlight_pred(image_files, df_none, max_cols)
 
-    #------------------------------------------------------------------------------#
+    #----------------------------- Chipping Predictions ---------------------------#
 
-    st.write(f"---\n## {len(df_unconfident)} Unconfident Predictions (< {threshold:.2%} Confidence)")
-    st.dataframe(df_unconfident)
-    st.write("")
-    highlight_pred(image_files, df_unconfident, max_cols)
+    with st.expander(f'View {len(df_chipping)} Chipping Predictions'):
+        st.dataframe(df_chipping)
+        st.write("")
+        highlight_pred(image_files, df_chipping, max_cols)
+
+    #-------------------------- Unconfident Predictions ---------------------------#
+
+    with st.expander(f'View {len(df_unconfident)} Unconfident Predictions (< {threshold:.2%} Confidence)'):
+        st.dataframe(df_unconfident)
+        st.write("")
+        highlight_pred(image_files, df_unconfident, max_cols)
