f4e435a09a640e41d6dd0535b20384d267fdbbf3
zhermin <tamzhermin@gmail.com>
2021-10-27 21:41:27 +0800

    hide st styles

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,9 +22,10 @@ hide_streamlit_style = """
 <style>
 #MainMenu {visibility: hidden;}
 footer {visibility: hidden;}
+.viewerBadge_container__1QSob {visibility: hidden;}
 </style>
 """
-# st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 #----------------------------------- Constants --------------------------------#
 
